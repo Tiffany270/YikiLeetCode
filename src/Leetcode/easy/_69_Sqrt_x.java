9ab6e17f238166b0da2b992baeabd6af432c7ae9
yiki <yiki270@foxmail.com>
2020-12-16 15:38:25 +0800

    '二分又一个'

---
+++ src/Leetcode/easy/_69_Sqrt_x.java
@@ -0,0 +1,26 @@
+package Leetcode.easy;
+
+public class _69_Sqrt_x {
+    public int mySqrt(int x) {
+
+        int left = 0;
+        int right = x;
+        int res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x) {
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+
+            }
+
+
+        }
+
+        return res;
+
+
+    }
+}
