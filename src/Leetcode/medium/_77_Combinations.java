c02af7418bd4b1eb6d8d77144d391ef82fb09511
yiki <yiki270@foxmail.com>
2021-02-19 17:40:11 +0800

    '回溯大法'

---
+++ src/Leetcode/medium/_77_Combinations.java
@@ -0,0 +1,53 @@
+package Leetcode.medium;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/*
+
+    Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+    You may return the answer in any order.
+
+                Example 1:
+                Input: n = 4, k = 2
+                Output:
+                [
+                  [2,4],
+                  [3,4],
+                  [2,3],
+                  [1,2],
+                  [1,3],
+                  [1,4],
+                ]
+
+                Example 2:
+                Input: n = 1, k = 1
+                Output: [[1]]
+* */
+public class _77_Combinations {
+
+    List<List<Integer>> res = new ArrayList<>();
+    Deque<Integer> path = new ArrayDeque<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        dfs(n, k, 1);
+        return res;
+    }
+
+    public void dfs(int n, int k, int startIdex) {
+
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = startIdex; i <= n; i++) {
+            path.addLast(i);
+            dfs(n, k, i + 1);
+            path.removeLast();
+
+        }
+    }
+}
